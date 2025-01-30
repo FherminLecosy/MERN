6637f17f21b8c5faa3506eceba5d3a5fa4181376
fherminlecosy <fahumada@lecosy.com.mx>
2025-01-30 11:38:06 -0700

    Se realizo el Register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,32 @@
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import  Logo  from '../components/Logo';
+import FormRow from '../components/FormRow';    
+
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register Page</h1>
-            <Link to='/login' className='btn'>Login</Link>
-        </div>
+        <Wrapper>
+            <form className='form'>
+                <Logo />
+                <h4>Register</h4>
+                <FormRow type="text" name="name" defaultValue= 'John' labelText="name" />
+                <FormRow type="text" name="lastName" defaultValue= 'Doe' labelText="last name" />
+                <FormRow type="text" name="location" defaultValue= 'johndoe@example.com' />
+                <FormRow type="password" name="password" defaultValue= '' />
+
+                <button type="submit" className="btn btn-block">
+                    submit
+                </button>
+                <p>
+                    Already have an account?
+                    <Link to="/login" className="member-btn">
+                        Login
+                    </Link>
+                </p>
+
+            </form>
+        </Wrapper>
 
     );
 };
